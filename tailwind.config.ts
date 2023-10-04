3529696e334ce40565fad1d614286c702bd06155
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-04 12:52:48 -0400

    added tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,12 @@ import type { PluginAPI } from "tailwindcss/types/config";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class", "[data-theme='dark']"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwindcss-themer")({}),
+  ],
 } satisfies Config;
