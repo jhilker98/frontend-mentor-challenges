a379cceb9adde92063f93b44d4382a59bff3a83c
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-04 15:28:41 -0400

    added empty content array to advice-generator.config.ts

--- src/styles/advice-generator.config.ts
+++ src/styles/advice-generator.config.ts
@@ -5,6 +5,7 @@ import type { PluginAPI } from "tailwindcss/types/config";
 
 export default {
   name: "advice-generator",
+  content: [],
   selectors: [".advice-generator", "[data-theme='advice-generator']"],
   screens: {
     mobile: "375px",
