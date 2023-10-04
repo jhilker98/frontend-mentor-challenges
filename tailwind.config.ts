d3e7eccdd7ff89dcbec3326b9220f4da26d096bf
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-04 14:06:11 -0400

    added advice generator theme to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,35 @@ export default {
   },
   plugins: [
     require("@tailwindcss/typography"),
-    require("tailwindcss-themer")({}),
+    require("tailwindcss-themer")({
+      defaultTheme: {
+        extend: {
+          // ...
+        },
+      },
+      themes: [
+        {
+          name: "advice-generator",
+          screens: {
+            mobile: "375px",
+            desktop: "1440px",
+          },
+          extend: {
+            colors: {
+              "light-cyan": "hsl(193, 38%, 86%)",
+              "neon-green": "hsl(150, 100%, 66%)",
+              "dark-blue": "hsl(218, 23%, 16%)",
+              "grayish-blue": {
+                DEFAULT: "hsl(217, 19%, 38%)",
+                dark: "hsl(217, 19%, 24%)",
+              },
+            },
+            fontFamily: {
+              sans: ['"Manrope"', ...defaultTheme.fontFamily.sans],
+            },
+          },
+        },
+      ],
+    }),
   ],
 } satisfies Config;
