0cac770da38f80fad45536a975ad0deda3164647
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-05 18:28:54 -0400

    created types.ts, set up consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,38 @@
+import { type Link, type SocialLink, type SiteConfig } from "~/types";
+
+const SOCIAL_LINKS: SocialLink[] = [
+  {
+    icon: "socials/fa6-brands/gitlab",
+    name: "GitLab",
+    href: "https://gitlab.com/jhilker98/",
+  },
+  {
+    icon: "socials/fa6-brands/github",
+    name: "GitHub",
+    href: "https://github.com/jhilker98/",
+  },
+  {
+    icon: "socials/fa6-brands/linkedin-in",
+    name: "LinkedIn",
+    href: "https://linked.com/in/jhilker/",
+  },
+  {
+    icon: "socials/fa6-brands/dev",
+    name: "Dev.to",
+    href: "https://dev.to/jhilker",
+  },
+  {
+    icon: "socials/fa6-brands/codepen",
+    name: "CodePen",
+    href: "https://codepen.io/hilkerj/",
+  },
+];
+
+export const SITE_META: SiteConfig = {
+  title: "Frontend Mentor Challenges",
+  description: "My frontendmentor.io challenges, all on one site.",
+  author: "Jacob Hilker",
+  tagline:
+    "A front-end developer, who enjoys using Emacs, Hugo, Python, and Tailwind.",
+  socialLinks: SOCIAL_LINKS,
+};
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+interface LinkProps {
+  name: string;
+  children?: undefined;
+  href: string;
+}
+interface ContainerProps {
+  name: string;
+  children: LinkProps[];
+  href?: undefined;
+}
+
+export type Link = LinkProps | ContainerProps;
+
+export type SocialLink = Omit<Link, "children"> & {
+  href: string;
+  icon: string;
+};
+
+export interface SiteConfig {
+  title: string;
+  description: string;
+  author: string;
+  tagline: string;
+  socialLinks?: SocialLink[];
+}
