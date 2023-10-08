b3eeb24094d77d7981d2b15166639352c6e04f52
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-08 14:04:35 -0400

    create space-tourism-site

--- src/styles/space-tourism-site.config.ts
+++ src/styles/space-tourism-site.config.ts
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
 import type { Config } from "tailwindcss";
-import type { PluginAPI } from "tailwindcss/types/config";
+//import type { PluginAPI } from "tailwindcss/types/config";
 
 export default {
   name: "space-tourism-site",
   content: [],
   extend: {
     // TODO finish space site
+    fontFamily: {
+      barlow: {
+        DEFAULT: ['"Barlow"', ...defaultTheme.fontFamily.sans],
+        condensed: ['"Barlow Condensed"', ...defaultTheme.fontFamily.sans],
+      },
+      bellefair: ['"Bellefair"', ...defaultTheme.fontFamily.serif],
+    },
   },
 } satisfies Config;
