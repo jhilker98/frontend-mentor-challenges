e5673da266e87643b6448427a056eda758080228
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-17 10:58:37 -0400

    added react to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://frontend-mentor-challenges.jhilker.com",
   outDir: "public",
   publicDir: "static",
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), react()]
 });
